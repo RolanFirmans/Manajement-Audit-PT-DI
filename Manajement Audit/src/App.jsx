a279bda6401aaf4e99f8a04c070bc42c057c9b52
zheivgt <133962917+zheivgt@users.noreply.github.com>
2024-07-30 14:25:01 +0700

    update

--- Manajement Audit/src/App.jsx
+++ Manajement Audit/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+<<<<<<< HEAD
 import LandingPage from "../src/LandingPage/LandingPage.jsx";
 import About from "../src/LandingPage/About.jsx";
 import Login from "../src/Login/Login.jsx";
+=======
+import LandingPage from "./LandingPage.jsx";
+import About from "./About.jsx";
+import AboutRoles from "./AboutRoles.jsx";
+>>>>>>> 8c38e30 (Update AboutRoles)
 import { Route, Routes } from "react-router-dom";
 import Admin from "./Admin/Admin.jsx";
 import AdminAuditIT from "./Admin Audit IT/AdminAuditIT.jsx";
@@ -15,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="About" element={<About />} />
+<<<<<<< HEAD
         <Route path="Login" element={<Login />} />
         <Route path="Admin" element={<Admin />} />
         <Route path="AdminAuditIT" element={<AdminAuditIT />} />
@@ -22,6 +29,9 @@ function App() {
         <Route path="Auditor" element={<Auditor />} />
         <Route path="Auditee" element={<Auditee/>} />
 
+=======
+        <Route path="AboutRoles" element={<AboutRoles />} />
+>>>>>>> 8c38e30 (Update AboutRoles)
       </Routes>
     </div>
   );
